06ecb3fac25c457745c421599de02e095f2af1f6
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-08-21 09:11:20 +0100

    styling the registration form

--- register.php
+++ register.php
@@ -4,9 +4,11 @@
 <html>
 <head>
     <title>Registration</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <script src="register.js"></script> 
 </head>
 <body>
+<div class="register-form">
     <h2>Registration</h2>
     <form action="index.php" method="post" onsubmit="return validateForm();">
         <input type="text" name="username" placeholder="Username" required><br>
@@ -15,5 +17,6 @@
         <input type="password" name="password" placeholder="Password" required><br>
         <button type="submit">Register</button>
     </form>
+</div>
 </body>
 </html>
