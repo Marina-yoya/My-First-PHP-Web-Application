4d5866d527374dd5d8b7db34c6b8b367cca411ad
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-08-21 10:48:36 +0100

    client-side validation for the registration

--- register.js
+++ register.js
@@ -1,3 +1,31 @@
-function validateForm() {
-    console.log("validate")
+const form = document.getElementById("form");
+const registerBtn = document.querySelector("button[type='submit']");
+registerBtn.addEventListener("click", validateForm);
+
+function validateForm(e) {
+
+    let username = document.querySelector("[name='username']").value;
+    let email = document.querySelector("[name='email']").value;
+    let phone = document.querySelector("[name='phone']").value;
+    let password = document.querySelector("[name='password']").value;
+
+    const usernameError = document.getElementById("username-error");
+    const emailError = document.getElementById("email-error");
+    const phoneError = document.getElementById("phone-error");
+    const passwordError = document.getElementById("password-error");
+
+    usernameError.textContent = username === "" ? "Username must be filled out" : "";
+    emailError.textContent = email === "" ? "Email must be filled out" : "";
+    phoneError.textContent = phone === "" ? "Phone must be filled out" : "";
+    passwordError.textContent = password === "" ? "Password must be filled out" : "";
+
+    if (username === "" || email === "" || phone === "" || password === "") {
+        e.preventDefault();
+    } else {
+
+        form.submit();
+    }
 }
+
+
+
--- register.php
+++ register.php
@@ -5,18 +5,31 @@
 <head>
     <title>Registration</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="register.js"></script> 
+    <script src="register.js" defer></script> 
 </head>
 <body>
-<div class="register-form">
-    <h2>Registration</h2>
-    <form action="index.php" method="post" onsubmit="return validateForm();">
+    <div class="register-form">
+        <h2>Registration</h2>
+        <form id="form" action="index.php" method="post" onsubmit="return validateForm();">
+       
         <input type="text" name="username" placeholder="Username" required><br>
+        <span id="username-error" class="error-message"></span> 
+        
         <input type="email" name="email" placeholder="Email" required><br>
+        <span id="email-error" class="error-message"></span> 
+        
         <input type="text" name="phone" placeholder="Phone" required><br>
+        <span id="phone-error" class="error-message"></span> 
+        
         <input type="password" name="password" placeholder="Password" required><br>
+        <span id="password-error" class="error-message"></span> 
+        
         <button type="submit">Register</button>
     </form>
-</div>
+    </div>
 </body>
 </html>
+
+
+
+
