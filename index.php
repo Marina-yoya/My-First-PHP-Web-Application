111c88f3a09134b9d701b24ee7d402bdc3979537
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-08-22 12:04:09 +0100

    sanitize user inputs to prevent XSS

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db = new PDO("mysql:host=localhost;dbname=myfirstdatabase", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $phone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
     $query = "INSERT INTO users (username, email, password, phone) VALUES (?, ?, ?, ?)";
